8bb74f81cffbafaf1246857fa4bb3d4263832a2d
Ohjelmistokehitys <Ohjelmistokehitys@OpiFrame6-THINK>
2014-11-27 14:01:22 +0200

    Chat Server V0.3

--- src/chatserver/ChatServer.java
+++ src/chatserver/ChatServer.java
@@ -21,27 +21,27 @@ public class ChatServer {
     
     public static void main(String[] args) {
         try {
-            //Start the server to listen port 3010
+            // Käynnistetään serveri kuuntelemaan porttia 3010
             ServerSocket server = new ServerSocket(3010);
             
-            //Start to listen and wait connections
-            while(true){
-                Socket temp = server.accept(); // Luo serveripäähän joka clientille oman socketin
-                ServerClientBackEnd backEnd = new ServerClientBackEnd(temp);
-                clients.add(backEnd);
-                Thread t = new Thread(backEnd);
-                t.setDaemon(true);              // Toissijainen säie, Java Virtual Machine tuhoaa tämän.
-                t.start();
-            }
-        } catch (IOException ex) {
+            // Käynnistetään kuuntelemaan ja odottamaan yhteyksiä****************************************
+            while(true){                                                                              //*
+                Socket temp = server.accept(); // Luo serveripäähän jokaiselle clientille oman socketin *
+                ServerClientBackEnd backEnd = new ServerClientBackEnd(temp);                          //*
+                clients.add(backEnd);                                                                 //*
+                Thread t = new Thread(backEnd);                                                       //*
+                t.setDaemon(true);              // Toissijainen säie, Java Virtual Machine tuhoaa tämän.*
+                t.start();                      // Ajetaan run-funktio server-päässä                    *
+            }//******************************************************************************************
+        } catch (IOException ex) {                                                                    
             ex.printStackTrace();
         }
     }
     
     public static void broadcastMessage(ChatMessage cm){
         
-        for(ServerClientBackEnd temp: clients){
-            temp.sendMessage(cm);
+        for(ServerClientBackEnd temp: clients){     // Käydään läpi kaikki clientit
+            temp.sendMessage(cm);                   // ja lähetetään niille viesti
         }
         
     }
